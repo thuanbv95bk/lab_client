6c66bca7c7745101d7c539d0835886e5d0df890a
Bui Van Thuan (PPM) <thuanbv@ba.vn>
2025-04-17 17:41:55 +0700

    add file

--- document/lab_3_select_db.sql
+++ document/lab_3_select_db.sql
@@ -19,22 +19,29 @@ WHERE 1 = 1
       AND (a.IsActived = 1);
 GO
 
-SELECT * FROM [Admin.UserVehicleGroup] A
-WHERE
-  ( A.IsDeleted IS NULL OR A.IsDeleted =0)
+SELECT *
+FROM [Admin.UserVehicleGroup] A
+WHERE (
+          A.IsDeleted IS NULL
+          OR A.IsDeleted = 0
+      );
 GO
 --'C77386A5-30A2-4793-BEE4-50DFABDEBE91'
 
-SELECT 
-	a.FK_CompanyID, 
-	a.PK_VehicleGroupID ,
-	a.ParentVehicleGroupID,
-	a.IsDeleted
-FROM dbo.[Vehicle.Groups] a WHERE
-						 a.FK_CompanyID =15076
-					and	( A.IsDeleted IS NULL OR A.IsDeleted =0)
-					--AND EXISTS (SELECT 1 FROM dbo.[Admin.Users] b WHERE b.PK_UserID ='C77386A5-30A2-4793-BEE4-50DFABDEBE91' AND b. )
+SELECT a.FK_CompanyID,
+       a.PK_VehicleGroupID,
+       a.ParentVehicleGroupID,
+       a.IsDeleted
+FROM dbo.[Vehicle.Groups] a
+WHERE a.FK_CompanyID = 15076
+      AND
+      (
+          a.IsDeleted IS NULL
+          OR a.IsDeleted = 0
+      );
+--AND EXISTS (SELECT 1 FROM dbo.[Admin.Users] b WHERE b.PK_UserID ='C77386A5-30A2-4793-BEE4-50DFABDEBE91' AND b. )
 GO
+
 SELECT G.*
 FROM dbo.[Vehicle.Groups] G
     JOIN dbo.[Admin.UserVehicleGroup] A
@@ -48,21 +55,21 @@ WHERE A.FK_UserID = 'C77386A5-30A2-4793-BEE4-50DFABDEBE91'
       AND ISNULL(G.IsDeleted, 0) = 0
       AND ISNULL(A.IsDeleted, 0) = 0;
 
-go
+GO
 
 SELECT a.*
 FROM dbo.[Vehicle.Groups] a
 WHERE ISNULL(a.IsDeleted, 0) = 0
-AND a.FK_CompanyID = 15076
- AND NOT EXISTS
+      AND a.FK_CompanyID = 15076
+      AND NOT EXISTS
 (
     SELECT 1
     FROM dbo.[Admin.UserVehicleGroup] b
     WHERE b.FK_VehicleGroupID = a.PK_VehicleGroupID
           AND ISNULL(b.IsDeleted, 0) = 0
           AND b.FK_UserID = 'C77386A5-30A2-4793-BEE4-50DFABDEBE91'
-)
-GO 
+);
+GO
 
 
 SELECT a.*
@@ -75,6 +82,7 @@ WHERE ISNULL(a.IsDeleted, 0) = 0
       AND a.FK_CompanyID = 15076
       AND b.FK_VehicleGroupID IS NULL;
 
+GO
 
 SELECT *
 FROM dbo.[Vehicle.VehicleGroups] A
