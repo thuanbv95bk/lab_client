8e73b41526e6012c56ecbd52690af20ec5873b40
Bui Van Thuan (PPM) <thuanbv@ba.vn>
2025-04-21 08:25:02 +0700

    chỉnh sửa sau demo

--- backend/App.Lab/obj/Debug/net8.0/App.Lab.AssemblyInfo.cs
+++ backend/App.Lab/obj/Debug/net8.0/App.Lab.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App.Lab")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f08f3bcadf01d29c58eff525e99a3b8e56bec71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22c098cf5fb6d9401cbdc04b177e90e8f9f3ba08")]
 [assembly: System.Reflection.AssemblyProductAttribute("App.Lab")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App.Lab")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
